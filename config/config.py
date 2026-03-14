9f8efe984f4aab7dd677a89a344083db5a21d21e
AnaGonzalezGuerra <glezguer.ana@gmail.com>
2026-03-14 09:54:01 +0100

    feat: add application entry point for FastAPI

--- config/config.py
+++ config/config.py
@@ -1,5 +1,4 @@
 """Configuration settings for the application."""
-import os
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -12,6 +11,12 @@ class Settings(BaseSettings):
     DB_HOST: str = ""
     DB_PORT: int = 0
     DB_NAME: str = ""
+    
+    # CORS settings
+    origins_urls: str = ""  # Comma-separated list of allowed origins for CORS
+    # Server settings
+    ip_server: str = ""
+    ip_port: int = 0
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent / ".env",
@@ -23,3 +28,13 @@ class Settings(BaseSettings):
         """Construct the async database URL from settings."""
         return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
                 f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
+    
+    @property
+    def origins_list(self) -> list[str]:
+        """Return origins as a list."""
+        return self.origins_urls.split(',') if self.origins_urls else []
+    
+    @property
+    def server_config(self) -> tuple[str, int]:
+        """Return server configuration as a tuple."""
+        return self.ip_server, self.ip_port
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 """Main application entry point."""
+# General libraries
+
+# FastAPI associated libraries
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+# My imports
+from routers.v1.routers import api_router
 from config.config import Settings
 
 settings = Settings()
 
+app = FastAPI(docs_url="/docs", redoc_url="/redoc")
+origins = settings.origins_list
+ip_server, ip_port = settings.server_config
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+# Include the API routes, with a prefix to version the API as v1.
+app.include_router(api_router, prefix="/api/v1")
+
 if __name__ == "__main__":
-    print("DB URL =>", settings.database_url)
-    print("DB HOST =>", settings.DB_HOST)
+   
+    uvicorn.run("main:app", host=ip_server, port=ip_port, reload=True)
