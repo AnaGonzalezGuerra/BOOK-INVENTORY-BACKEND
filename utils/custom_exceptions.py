4f36d2d0bfcd463afdca27ea73ec5f4df06530b9
AnaGonzalezGuerra <glezguer.ana@gmail.com>
2026-03-28 22:59:09 +0100

    refactor: replace multiple error classes with a unified CustomException for better error handling

--- utils/custom_exceptions.py
+++ utils/custom_exceptions.py
@@ -1,40 +1,22 @@
-class error_400(Exception):
-    def __init__(self, message="400 Bad Request"):
-        self.message = message
-        super().__init__(self.message)
+"""Custom exceptions for the application."""
+from fastapi import HTTPException
 
 
-class error_401(Exception):
-    def __init__(self, message="401 Unauthorized"):
-        self.message = message
-        super().__init__(self.message)
-
-
-class error_403(Exception):
-    def __init__(self, message="403 Forbidden"):
-        self.message = message
-        super().__init__(self.message)
-
-
-class error_404(Exception):
-    def __init__(self, message="404 Not Found"):
-        self.message = message
-        super().__init__(self.message)
-
-
-class error_409(Exception):
-    def __init__(self, message="409 Conflict"):
-        self.message = message
-        super().__init__(self.message)
-
-
-class error_415(Exception):
-    def __init__(self, message="415 Unsupported Media Type"):
-        self.message = message
-        super().__init__(self.message)
-
-
-class error_500(Exception):
-    def __init__(self, message="500 Internal server error"):
-        self.message = message
-        super().__init__(self.message)
+class CustomException(HTTPException):
+    """Custom HTTP exception for API errors."""
+    
+    def __init__(self, status_code: int, message: str, error_code: str = "ERROR"):
+        """
+        Initialize custom exception.
+        
+        Args:
+            status_code: HTTP status code
+            message: Error message (visible to client in Spanish)
+            error_code: Internal error code for debugging
+        """
+        self.error_code = error_code
+        detail = {
+            "error_code": error_code,
+            "message": message
+        }
+        super().__init__(status_code=status_code, detail=detail)
